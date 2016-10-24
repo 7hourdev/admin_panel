4a3fef1d1ab36f5bf1d20e94bab725ddcf960523
Jefftree <jeffrey.ying86@live.com>
2016-10-23 18:14:04 -0700

    stormpath

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ var webpack = require('webpack');
 var webpackDevMiddleware = require("webpack-dev-middleware");
 var webpackHotMiddleware = require("webpack-hot-middleware");
 var webpackConfig = require('./../webpack.config');
+var config = require('../config');
 
 var app = express();
 var compiler = webpack(webpackConfig);
@@ -25,6 +26,39 @@ app.use(bodyParser.json());
 
 // serve our static stuff like index.css
 app.use(express.static(path.join(__dirname, '../public')));
+
+var stormpath = require("express-stormpath");
+var stormpath_client = require("stormpath");
+
+var User = require('./models/user');
+
+app.use(stormpath.init(app, {
+    apiKey: {
+        id: config.stormpath.apiKey.id,
+        secret: config.stormpath.apiKey.secret
+    },
+    application: {
+        href: config.stormpath.application
+    },
+    website: true,
+    debug: "info",
+    web: {
+        login: {
+            enabled: true,
+            nextUri: "/dashboard"
+        }
+    },
+    postRegistrationHandler: function (account, req, res, next) {
+        console.log(account);
+        var user = User.build({
+            username: account.username,
+            email: account.email,
+        });
+        user.save();
+        next();
+    }
+}));
+app.use(stormpath.getUser);
 
 // custom routes
 var api_route = require('./routes/api');
