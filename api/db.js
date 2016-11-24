28bab98a20f84b8d1be4983b0d6c1fb2d660af22
david <david-lky@hotmail.com>
2016-11-23 23:30:47 -0800

    Updated

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ var Sequelize = require("sequelize");
 var config = require("../config").db;
 
 var sequelize = new Sequelize(config.name, config.user, config.pass, {
-  host: "localhost",
+  host: config.url,
   port: 5432,
   dialect: "postgres",
   logging: (process.argv.length >= 2 && process.argv[2] == "test") ? false : console.log
--- api/db_init.js
+++ api/db_init.js
@@ -9,25 +9,8 @@ Model.User.belongsToMany(Model.Site, {
     through: "UserSite"
 });
 Model.Site.hasMany(Model.Content);
-Model.Content.belongsTo(Model.Site);
+Model.Content.belongsTo(Model.Site);    
 
 Sequelize.sync({force: true}).then(function() {
-    //Sequelize.sync().then(function() {
-
     console.log('DB Sync Completed');
-
-    var dummy = Model.Site.build({
-        name: '7hourdev',
-        url: 'http://7hourdev.com',
-        contents: [{
-            name: 'about',
-            content: 'we did good'
-        }, {
-            name: 'home',
-            content: 'sup'
-        }]
-    }, {
-        include: [Model.Content]
-    })
-    dummy.save();
 });
