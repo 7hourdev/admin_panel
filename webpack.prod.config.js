46dc95653c851923eb0ba4a69dea1e9a8526e3a8
David Liu <david-lky@hotmail.com>
2017-12-07 01:39:51 -0500

    fixed url

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ webpackConfig.plugins = [
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
-        'URL': JSON.stringify('https://portal.7hourdev.com'),
+        'URL': JSON.stringify('//portal.7hourdev.com'),
       }
     }),
     new CompressionPlugin({
